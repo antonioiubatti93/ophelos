8d04462d3df28949593f65571d70e8af84efd5fc
Antonio Iubatti <aiubatti@edgelab.ch>
2023-05-19 16:02:51 +0200

    feat: interest rate (#5)

---
+++ interest/rate_test.go
@@ -0,0 +1,45 @@
+package interest
+
+import (
+	"math"
+	"testing"
+
+	"github.com/antonioiubatti93/ophelos/termstructure"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_Rate_Discount(t *testing.T) {
+	t.Parallel()
+
+	const (
+		tm  = 0.5
+		tol = 1.0e-15
+	)
+
+	for _, tc := range []struct {
+		name     string
+		rate     Rate
+		expected float64
+	}{
+		{
+			"constant term structure/simple discount",
+			NewRate(termstructure.NewFlat(0.01), NewSimpleDiscount()),
+			1.0 / 1.005,
+		},
+		{
+			"parametric term structure/continuous discount",
+			NewRate(termstructure.TermStructureFunc(func(t float64) float64 {
+				return math.Log(1.01) / t
+			}), NewContinuousDiscount()),
+			1.0 / 1.01,
+		},
+	} {
+		tc := tc
+
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			assert.InDelta(t, tc.expected, tc.rate.Discount(tm), tol)
+		})
+	}
+}
